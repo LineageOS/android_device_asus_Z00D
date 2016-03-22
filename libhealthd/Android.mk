495689356ed811e7bdead35e3a13dbb11cbe13f6
artefvck <artefvck07@gmail.com>
2016-03-22 17:35:57 +0700

    cvtlp : healtd:fix invalid source file

--- libhealthd/Android.mk
+++ libhealthd/Android.mk
@@ -16,7 +16,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := healthd_board_clovertrail
+LOCAL_SRC_FILES := healthd_board_clovertrail.cpp
 LOCAL_MODULE := libhealthd.clovertrail
 LOCAL_C_INCLUDES := system/core/healthd
 LOCAL_CFLAGS := -Werror
