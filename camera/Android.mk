840f33927d4e624fd4f0c05fd038cf635e0aa2ba
artefvck <artefvck07@gmail.com>
2016-05-04 22:49:48 +0700

    common : fix include header for loading camera_client

--- camera/Android.mk
+++ camera/Android.mk
@@ -10,6 +10,7 @@ LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils
 
 LOCAL_C_INCLUDES := \
+    system/core/include \
     system/media/camera/include
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
