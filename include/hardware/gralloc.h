3b60d34e26ea5fcdea3dab795c23224f159fe193
Flex1911 <dedsa2002@gmail.com>
2016-12-07 18:07:24 +0300

    Fixed old-cast in gralloc header

--- include/hardware/gralloc.h
+++ include/hardware/gralloc.h
@@ -376,7 +376,7 @@ typedef struct alloc_device_t {
 static inline int gralloc_open(const struct hw_module_t* module, 
         struct alloc_device_t** device) {
     return module->methods->open(module, 
-            GRALLOC_HARDWARE_GPU0, (struct hw_device_t**)device);
+            GRALLOC_HARDWARE_GPU0, reinterpret_cast<hw_device_t**>(device));
 }
 
 static inline int gralloc_close(struct alloc_device_t* device) {
