25cc652e37bba42a7dea55275c475f445f2b1771
Flex1911 <dedsa2002@gmail.com>
2016-12-09 16:07:28 +0300

    Fixes for gps and init

--- rootdir/etc/gps_daemon.sh
+++ rootdir/etc/gps_daemon.sh
@@ -1,4 +1,4 @@
 #!/system/bin/sh
 # software configuration related boot actions
 # shellscript as this is the only way selinux will allow this to proceed
-/system/bin/gpsd -c /system/etc/gpsconfig.xml
+/system/bin/gpsd -c /system/etc/gps.xml
