565c09751a7dbe83b46952234fa0fdb12a4c6c7b
Flex1911 <dedsa2002@gmail.com>
2017-03-05 12:53:18 +0300

    Z00D: enable sdcardfs
    Change-Id: If5d07098325d9a21a55d4d6ad4710bde007d4438

--- device.mk
+++ device.mk
@@ -205,6 +205,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libstlport
 
+# Sdcardfs
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.sdcardfs=true
+
 # Sensors
 PRODUCT_PACKAGES += \
     libshim_sensors
